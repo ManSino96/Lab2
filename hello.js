645e9eb378430518f369671454502e81c4ece7af
Sino Mazibuko <mazisino@icloud.com>
2020-03-04 13:07:04 +0200

    Temperature Conversion

--- hello.js
+++ hello.js
@@ -1,2 +1,5 @@
-const message = 'Hello World!'
-console.log(message)
+const temp_converter = function(temp){
+ return (temp-32)*(5/9)
+}
+const result = temp_converter(100)
+console.log(`Temperature conversion ${result} is degrees`)
